be37001234613c2a6a1412e04e9818fc1863e98e
Paula <panna.na@gmail.com>
2023-04-27 17:28:19 -0400

    readme-file-added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,16 @@
-import { Link } from "react-router-dom";
-import { logOut } from "../utilities/users-service";
+//import { Link } from "react-router-dom";
+//import { logOut } from "../utilities/users-service";
+import { AppBar, Toolbar } from "@mui/material";
 
-function NavBar({ user, setUser }) {
-  const handleLogOut = () => {
-    logOut();
-    setUser(null);
-  };
-  return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link> <span>Welcome, {user.name}</span>{" "}
-      <Link to="" onClick={handleLogOut}>
-        Logout
-      </Link>
-    </nav>
-  );
+
+const NavBar= ()=>{
+  return(
+    <AppBar>
+       <Toolbar>
+
+       </Toolbar>
+       <AppBar/>
+  )
 }
 
 export default NavBar;
